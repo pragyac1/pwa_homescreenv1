bbb6969ea5252dbc518a326504fcb343333e2b12
pragyac1 <43171645+pragyac1@users.noreply.github.com>
2018-11-05 14:18:50 +0100

    Add files via upload

---
+++ pwa_homescreenv1/main.js
@@ -0,0 +1,5 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('service.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
